b740ffdfd4f517d5e7ef440efc6e47efd46c37f9
kayahakan52 <egelih_kaya@yahoo.com>
2020-07-24 00:09:06 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	HelloWorldProject/src/main/java/HelloWorld.java

--- HelloWorldProject/src/main/java/HelloWorld.java
+++ HelloWorldProject/src/main/java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Github");
         System.out.println("Hello Github 2nd line");
-        System.out.println("3rd line added using win terminal");
+        System.out.println("new 3rd line added using win terminal");
+
     }
 }
